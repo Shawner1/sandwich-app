02c4db27945c7629de06d73699a0a6cf9d4dab4b
Shawner1 <ShawnHing12@gmail.com>
2022-04-13 06:50:35 -0400

    sandwich

---
+++ sandwich/sandwichapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import Http404
+from django.views import View
+import random
+
+ingredients = {
+    'meats': ['corned beef', 'pastrami', 'honey turkey', 'pepper steak', 'veggie burger'],
+    'cheeses': ['american', 'swiss', 'provolone', 'cheddar', 'mozzarella'],
+    'toppings': ['lettuce', 'tomato', 'onions', 'peppers', 'pickles']
+}
+
+
+class SandwichappView(View):
+    def get(self, request):
+        return render(
+            request = request,
+            template_name = 'sandwichapp.html',
+            context = {'ingredients': ingredients.keys()}
+        )
+
+
+class IngredientsListView(View):
+    def get(self, request, ingredient_type):
+        if ingredient_type not in ingredients:
+            raise Http404(f'No such ingredient: {ingredient_type}')
+
+        return render(
+            request = request,
+            template_name = 'ingredients_list.html',
+            context={ 'ingredients': ingredients[ingredient_type],
+                        'ingredient_type': ingredient_type }
+        )
+
+
+class SandwichGeneratorView(View):
+    def get(self, request):
+        selected_meat = random.choice(ingredients["meats"])
+        selected_cheese = random.choice(ingredients["cheeses"])
+        selected_topping = random.choice(ingredients["toppings"])
+
+        sandwich = f'{selected_meat} & {selected_cheese} with {selected_topping}'
+        return render(request, "sandwich_generator.html", context = {"sandwich" : sandwich }) 
+
+
+
